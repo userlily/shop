c643f92447d1ab66450de7cafe2e888bafd491c1
lili <296876606@qq.com>
2017-09-24 15:39:44 +0800

    commit

--- routes/wx/index.js
+++ routes/wx/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var login= require("./login");
 var aa= require("./aa")
 var tuijian= require("./tuijian")
+var serach= require("./search")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'wx' });
@@ -10,5 +11,6 @@ router.get('/', function(req, res, next) {
 router.use('/login',login)
 router.use('/aa',aa)
 router.use('/tuijian',tuijian)
+router.use('/serach',serach)
 
 module.exports = router;
