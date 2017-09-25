439239f64e52ad6bf0236364d755e292814c362a
lili <296876606@qq.com>
2017-09-25 17:48:45 +0800

    commit

---
+++ routes/wx/goodslist.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var pool = require("../../util/mysqlConnectionPool")
+
+/**
+ * 微信小程序登录
+ * @param  code
+ *
+ *
+ */
+router.get('/', function(req, res, next) {
+
+    var id= req.query.id;
+    pool.getConnection(function(err, connection) {
+
+        // Use the connection
+        connection.query("SELECT * FROM goods where cid =? ",id,function (error, results, fields) {
+            connection.release();
+            if(error) console.log(error);
+            res.json(results)
+        });
+    });
+
+});
+module.exports = router;
--- routes/wx/index.js
+++ routes/wx/index.js
@@ -6,6 +6,7 @@ var tuijian= require("./tuijian")
 var search= require("./search")
 var topclass= require("./topclass")
 var secondclass= require("./secondclass")
+var goodslist= require("./goodslist")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'wx' });
@@ -16,5 +17,6 @@ router.use('/tuijian',tuijian)
 router.use('/search',search)
 router.use('/topclass',topclass)
 router.use('/secondclass',secondclass)
+router.use('/goodslist',goodslist)
 
 module.exports = router;
